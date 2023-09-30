da3bac20fe85feb046273197686e2bca55225cf6
Ojaswi Chopra <99067527+ojaswichopra@users.noreply.github.com>
2023-10-01 02:33:44 +0530

    Update Article.jsx

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from 'react';
 import './article.css';
 
-const Article = () => {
-  return (
-    <div>
-      
+const Article = ({ imgUrl, date, text, Url }) => (
+  <div className="ws__blog-container_article">
+    <div className="ws__blog-container_article-image">
+      <img src={imgUrl} alt="blog_image" />
     </div>
-  )
-}
+    <div className="ws__blog-container_article-content">
+      <div>
+        <p>{date}</p>
+        <h3>{text}</h3>
+      </div>
+      <p> <a href={Url} target="_blank"> Read Full Article </a></p>
+    </div>
+  </div>
+);
 
-export default Article
+export default Article;
